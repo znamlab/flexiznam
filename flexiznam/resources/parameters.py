6a2f193b241e901f0928a61bd146cc73be5b541c
Antonin Blot <antonin.blot@gmail.com>
2021-04-23 16:25:09 +0100

    remove assertion on DONWLOAD_FOLDER

--- flexiznam/resources/parameters.py
+++ flexiznam/resources/parameters.py
@@ -1,5 +1,7 @@
 import os
+import warnings
 # Default values
+
 
 FLEXILIMS_USERNAME = 'blota'
 MCMS_USERNAME = 'ab8'
@@ -7,4 +9,5 @@ MCMS_USERNAME = 'ab8'
 
 # Config
 DOWNLOAD_FOLDER = os.path.join(os.path.expanduser('~'), 'Downloads')
-assert os.path.isdir(DOWNLOAD_FOLDER)
+if not os.path.isdir(DOWNLOAD_FOLDER):
+    warnings.warn('Download folder does not exist. Check you parameter file')
