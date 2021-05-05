6cdbeddcefc7d5352944d42d31c2b63c89c2e634
Antonin Blot <antonin.blot@gmail.com>
2021-05-05 17:12:44 +0100

    Make configuration errors more explicit

--- flexiznam/errors.py
+++ flexiznam/errors.py
@@ -3,3 +3,10 @@ class NameNotUniqueException(Exception):
     Mouse name does not correspond to exactly 1 entry
     """
     pass
+
+
+class ConfigurationError(Exception):
+    """
+    The configuration file cannot be loaded
+    """
+    pass
--- flexiznam/utils.py
+++ flexiznam/utils.py
@@ -1,11 +1,15 @@
+import os.path
 import pathlib
 import yaml
+from flexiznam.errors import ConfigurationError
 
 
 def load_param(param_file=None):
     """Read parameter file from config folder"""
     if param_file is None:
         param_file = pathlib.Path(__file__).parent.absolute() / 'config' / 'config.yml'
+    if not os.path.isfile(param_file):
+        raise ConfigurationError('Cannot find the configuration file')
     with open(param_file, 'r') as yml_file:
         prm = yaml.safe_load(yml_file)
     return prm
@@ -15,6 +19,8 @@ def get_password(username, app, password_file=None):
     """Read the password yaml"""
     if password_file is None:
         password_file = pathlib.Path(__file__).parent.absolute() / 'config' / 'secret_password.yml'
+    if not os.path.isfile(password_file):
+        raise ConfigurationError('Cannot find the password file')
     with open(password_file, 'r') as yml_file:
         pwd = yaml.safe_load(yml_file)
     if app not in pwd:
@@ -25,7 +31,9 @@ def get_password(username, app, password_file=None):
     return pwd[username]
 
 
-PARAMETERS = load_param()
-# expanduser for file paths:
-PARAMETERS['download_folder'] = pathlib.Path(PARAMETERS['download_folder']).expanduser()
-
+try:
+    PARAMETERS = load_param()
+    # expanduser for file paths:
+    PARAMETERS['download_folder'] = pathlib.Path(PARAMETERS['download_folder']).expanduser()
+except Exception:
+    raise ConfigurationError('Could not load the parameters. Check your configuration file')
