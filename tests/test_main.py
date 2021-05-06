818c0a2e5e6d2c37b1ab259d0264bbb733cf2faa
Antonin Blot <antonin.blot@gmail.com>
2021-05-06 18:29:16 +0100

    Mark tests to not be run on github

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,8 @@ import flexiznam
 from flexiznam.utils import PARAMETERS
 
 
+@pytest.mark.integtest
 def test_get_mice():
     mice_df = flexiznam.get_mice(project_id=PARAMETERS['project_ids']['test'])
     assert mice_df.shape == (1, 69)
+
