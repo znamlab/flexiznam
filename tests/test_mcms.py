622ecb698b077ac04e79c169998aa4924e5d17e4
Antonin Blot <antonin.blot@gmail.com>
2021-05-07 09:58:56 +0100

    Change tests to skip mcms tests by default

--- tests/test_mcms.py
+++ tests/test_mcms.py
@@ -4,13 +4,13 @@ from flexiznam import mcms
 USERNAME = 'ab8'
 
 
-@pytest.mark.integtest
+@pytest.mark.slow
 def test_download_mouse():
     ret = mcms.main.download_mouse_info(username=USERNAME, mouse_name='PZAJ2.1c')
     assert ret
 
 
-@pytest.mark.integtest
+@pytest.mark.slow
 def test_get_mouse_df():
     mcms.get_mouse_df(mouse_name='PZAJ2.1c', username=USERNAME)
 
