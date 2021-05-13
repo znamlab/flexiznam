60d81afab7581868f7d0e67ff94388a122874e26
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2021-05-13 10:26:23 +0100

    setup.py: add yaml to requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         'Click',
         'pandas',
         'webbot',
+        'yaml',
         'flexilims @ git+ssh://git@github.com/znamlab/flexilims.git#egg=flexilims'
     ],
     entry_points='''
