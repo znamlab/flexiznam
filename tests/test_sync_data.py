8d057bc83f0ce12d7553eead88f529015c904fca
Antonin Blot <antonin.blot@gmail.com>
2021-05-21 10:00:30 +0100

    Add CLI and test for sync

--- tests/test_sync_data.py
+++ tests/test_sync_data.py
@@ -2,8 +2,16 @@ import pytest
 import pathlib
 from flexiznam.camp import sync_data
 
-PATH_TO_YAML = pathlib.Path(__file__).parent.absolute() / '..' / 'flexiznam' / 'camp' / 'example_acquisition_yaml.yml'
+znm_folder = pathlib.Path(__file__).parent.absolute()
+PATH_TO_FULL_YAML = znm_folder / '..' / 'flexiznam' / 'camp' / 'example_acquisition_yaml.yml'
+PATH_TO_MINI_YAML = znm_folder / '..' / 'flexiznam' / 'camp' / 'minimal_example_acquisition_yaml.yml'
 
 
-def test_parse_yaml():
-    sync_data.clean_yaml(PATH_TO_YAML)
+def test_clean_yaml():
+    sync_data.clean_yaml(PATH_TO_MINI_YAML)
+    sync_data.clean_yaml(PATH_TO_FULL_YAML)
+
+
+def test_parse_yaml(tmpdir):
+    sync_data.parse_yaml(PATH_TO_MINI_YAML)
+    sync_data.parse_yaml(PATH_TO_FULL_YAML)
