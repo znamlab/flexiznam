1d97dc18af531bf97ea2d5f8e2622d25fed3975a
Antonin Blot <antonin.blot@gmail.com>
2021-05-25 11:57:54 +0100

    Update tests. All passing

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,6 +3,7 @@ import pathlib
 import pandas as pd
 from flexiznam.schema import Dataset, CameraData, HarpData, ScanimageData
 from flexiznam.config import PARAMETERS
+from flexiznam.errors import DatasetError
 from tests.tests_resources import acq_yaml_and_files
 
 
@@ -13,13 +14,32 @@ def test_dataset():
     names = [dict(mouse='mouse', session='S12345678', recording='R123456', dataset_name='name_with_underscore'),
              dict(mouse='mouse', session='S12345678_00', recording='R123456_00', dataset_name='name_with_underscore'),
              dict(mouse='mo_use', session='S12345678', recording='R123456', dataset_name='name_with_underscore'),
-             dict(mouse='mo_use', session='S12345678_123456', recording=None, dataset_name='name_with_underscore')]
+             dict(mouse='mo_use', session='S12345678_123456', recording=None, dataset_name='name_with_underscore')
+             ]
 
     for n in names:
         parts = [n[k] for k in parts_label]
         ds.name = '_'.join([p for p in parts if p is not None])
         for p in parts_label:
             assert getattr(ds, p) == n[p]
+
+    # test also a few unvalid datasets
+    bad_names = ['hopeles',
+                 'mo_use_S12345678_123456_000_norec',
+                 'mo_use_000_R123456_000_nosess']
+    msgs = ['Cannot parse dataset name. No match in: `hopeles`. Must be `<MOUSE>_SXXXXXX[...]_<DATASET>`.' +
+              '\nSet self.mouse, self.session, self.recording, and self.dataset_name individually',
+            'Cannot parse dataset name. Found recording number but not recording name in ' +
+              '`mo_use_S12345678_123456_000_norec`\nSet self.mouse, self.session, self.recording, ' +
+              'and self.dataset_name individually',
+            'Cannot parse dataset name. No match in: `mo_use_000_R123456_000_nosess`. Must be ' +
+              '`<MOUSE>_SXXXXXX[...]_<DATASET>`.\nSet self.mouse, self.session, self.recording, and ' +
+              'self.dataset_name individually']
+    for name, err_msg in zip(bad_names, msgs):
+        with pytest.raises(DatasetError) as exc:
+            ds.name = name
+        assert exc.value.args[0] == err_msg
+
 
 @pytest.mark.integtest
 def test_dataset_flexilims_integration():
@@ -54,9 +74,9 @@ def test_from_flexilims():
     ds = Dataset.from_flexilims(project, name='R101501_retinotopy_suite2p_traces')
 
 
-def test_camera(tmpdir):
-    acq_yaml_and_files.create_acq_files(tmpdir)
-    data_dir = tmpdir / acq_yaml_and_files.MOUSE / acq_yaml_and_files.SESSION / next(
+def test_camera(tmp_path):
+    acq_yaml_and_files.create_acq_files(tmp_path)
+    data_dir = tmp_path / acq_yaml_and_files.MOUSE / acq_yaml_and_files.SESSION / next(
         iter(acq_yaml_and_files.MINIAML['recordings'].keys()))
     ds = CameraData.from_folder(data_dir)
     assert len(ds) == 4
@@ -68,9 +88,9 @@ def test_camera(tmpdir):
     assert ds['face_camera'].name == 'testmouse_testsession_testrecording_face_camera'
 
 
-def test_harp(tmpdir):
-    acq_yaml_and_files.create_acq_files(tmpdir)
-    data_dir = tmpdir / 'PZAH4.1c/S20210513/ParamLog/R193432_Retinotopy'
+def test_harp(tmp_path):
+    acq_yaml_and_files.create_acq_files(tmp_path)
+    data_dir = tmp_path / 'PZAH4.1c/S20210513/ParamLog/R193432_Retinotopy'
     ds = HarpData.from_folder(data_dir)
     assert len(ds) == 1
     d = next(iter(ds.values()))
@@ -80,12 +100,13 @@ def test_harp(tmpdir):
 
 
 @pytest.mark.integtest
-def test_scanimage(tmpdir):
-    acq_yaml_and_files.create_acq_files(tmpdir)
-    data_dir = tmpdir / 'PZAH4.1c/S20210513/R193432_Retinotopy'
+def test_scanimage(tmp_path):
+    acq_yaml_and_files.create_acq_files(tmp_path)
+    data_dir = tmp_path / 'PZAH4.1c/S20210513/R193432_Retinotopy'
     ds = ScanimageData.from_folder(data_dir)
     assert len(ds) == 1
     d = next(iter(ds.values()))
+    assert d.name == 'PZAH4.1c_S20210513_R193432_Retinotopy00001'
     assert d.name == next(iter(ds.keys()))
     assert d.is_valid()
     assert len(d) == 39
--- tests/test_sync_data.py
+++ tests/test_sync_data.py
@@ -1,17 +1,57 @@
 import pytest
 import pathlib
+import yaml
 from flexiznam.camp import sync_data
+from flexiznam.config import PARAMETERS
+from tests.tests_resources import acq_yaml_and_files
 
-znm_folder = pathlib.Path(__file__).parent.absolute()
-PATH_TO_FULL_YAML = znm_folder / '..' / 'flexiznam' / 'camp' / 'example_acquisition_yaml.yml'
-PATH_TO_MINI_YAML = znm_folder / '..' / 'flexiznam' / 'camp' / 'minimal_example_acquisition_yaml.yml'
+def test_clean_yaml(tmp_path):
+    path_to_full_yaml = tmp_path / 'full_yaml.yml'
+    path_to_mini_yaml = tmp_path / 'mini_yaml.yml'
+    with open(path_to_mini_yaml, 'w') as minifile:
+        yaml.dump(acq_yaml_and_files.MINIAML, minifile)
+    with open(path_to_full_yaml, 'w') as fullfile:
+        yaml.dump(acq_yaml_and_files.FAML, fullfile)
+
+    sync_data.clean_yaml(path_to_mini_yaml)
+    sync_data.clean_yaml(path_to_full_yaml)
 
 
-def test_clean_yaml():
-    sync_data.clean_yaml(PATH_TO_MINI_YAML)
-    sync_data.clean_yaml(PATH_TO_FULL_YAML)
+def test_parse_yaml(tmp_path):
+    acq_yaml_and_files.create_acq_files(tmp_path)
+    path_to_full_yaml = tmp_path / 'full_yaml.yml'
+    path_to_mini_yaml = tmp_path / 'mini_yaml.yml'
+    with open(path_to_mini_yaml, 'w') as minifile:
+        yaml.dump(acq_yaml_and_files.MINIAML, minifile)
+    with open(path_to_full_yaml, 'w') as fullfile:
+        yaml.dump(acq_yaml_and_files.FAML, fullfile)
 
+    m = sync_data.parse_yaml(path_to_mini_yaml, raw_data_folder=tmp_path, verbose=False)
+    assert len(m) == 10
+    # there should not be any error
+    errs = sync_data.find_xxerrorxx(yml_data=m)
+    assert not errs
+    sess_data = sync_data.parse_yaml(path_to_full_yaml, raw_data_folder=tmp_path, verbose=False)
+    errs = sync_data.find_xxerrorxx(yml_data=sess_data)
+    assert len(errs) == 3
 
-def test_parse_yaml(tmpdir):
-    sync_data.parse_yaml(PATH_TO_MINI_YAML)
-    sess_data = sync_data.parse_yaml(PATH_TO_FULL_YAML, verbose=False)
+def test_write_yaml(tmp_path):
+    acq_yaml_and_files.create_acq_files(tmp_path)
+    path_to_full_yaml = tmp_path / 'full_yaml.yml'
+    with open(path_to_full_yaml, 'w') as fullfile:
+        yaml.dump(acq_yaml_and_files.FAML, fullfile)
+    sess_data = sync_data.parse_yaml(path_to_full_yaml, raw_data_folder=tmp_path, verbose=False)
+    target = tmp_path / 'target_out.yml'
+    pure_yaml = sync_data.write_session_data_as_yaml(session_data=sess_data, target_file=target)
+    def rec_test(d):
+        for k,v in d.items():
+            if isinstance(v, dict):
+                rec_test(v)
+            elif not isinstance(v, str):
+                if v is None:
+                    continue
+                raise IOError('Potentially unvalid yaml. It contains: %s' % v)
+    rec_test(pure_yaml)
+    with open(target, 'r') as reader:
+        reload = yaml.safe_load(reader)
+    assert reload == pure_yaml
