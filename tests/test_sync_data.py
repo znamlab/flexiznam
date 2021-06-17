6910d0e063c12e2839422147d00c6d5c1c712c81
Antonin Blot <antonin.blot@gmail.com>
2021-06-17 11:02:35 +0100

    Bugfix in tests: Remove conflicts attribute from upload_yaml

--- tests/test_sync_data.py
+++ tests/test_sync_data.py
@@ -74,12 +74,10 @@ def test_upload(tmp_path):
     with open(path_to_mini_yaml, 'w') as fullfile:
         yaml.dump(acq_yaml_and_files.MINIAML, fullfile)
 
-    sync_data.upload_yaml(source_yaml=path_to_mini_yaml, raw_data_folder=tmp_path,
-                          conflicts='overwrite')
+    sync_data.upload_yaml(source_yaml=path_to_mini_yaml, raw_data_folder=tmp_path)
     parsed = sync_data.parse_yaml(path_to_yaml=path_to_mini_yaml,
                                   raw_data_folder=tmp_path)
     path_to_parsed = tmp_path / 'parsed_mini_yaml.yml'
     sync_data.write_session_data_as_yaml(parsed, target_file=path_to_parsed,
                                          overwrite=True)
-    sync_data.upload_yaml(source_yaml=path_to_parsed, raw_data_folder=tmp_path,
-                          conflicts='overwrite')
+    sync_data.upload_yaml(source_yaml=path_to_parsed, raw_data_folder=tmp_path)
