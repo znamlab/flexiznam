92e2c1e5c862785c7f631b0628c914b57c29c241
Antonin Blot <antonin.blot@crick.ac.uk>
2023-05-18 23:51:26 +0100

    [bugfix] fix a forgotten ref to clean_recursively

--- flexiznam/schema/datasets.py
+++ flexiznam/schema/datasets.py
@@ -395,7 +395,7 @@ class Dataset(object):
                 attributes[attribute] = bool(attributes[attribute])
             if isinstance(attributes[attribute], tuple):
                 attributes[attribute] = list(attribute)
-        flz.camp.make_json_compatible(attributes)
+        flz.utils.clean_recursively(attributes)
 
         if status == "different":
             if mode == "safe":
