5816762ee72c9b301db6fc72fea71b8a189ccbbf
Antonin Blot <antonin.blot@crick.ac.uk>
2023-06-02 14:27:54 +0100

    [version] bump version umber of dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flexiznam",
-    version="v0.3.5",
+    version="v0.3.6",
     url="https://github.com/znamlab/flexznam",
     license="MIT",
     author="Antonin Blot",
