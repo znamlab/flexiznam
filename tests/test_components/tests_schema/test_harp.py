75246fb10548f41e1efb38c3e7d8a13fe9dae4f6
Antonin Blot <antonin.blot@crick.ac.uk>
2023-07-17 17:52:54 +0100

    [tests] fix test to change in demo data

--- tests/test_components/tests_schema/test_harp.py
+++ tests/test_components/tests_schema/test_harp.py
@@ -1,14 +1,13 @@
 import pytest
 from flexiznam.schema.harp_data import HarpData
 from tests.tests_resources.data_for_testing import DATA_ROOT
-from pathlib import Path
 
 
 def test_harp():
     folder_genealogy = ["mouse_physio_2p", "S20211102", "R165821_SpheresPermTube"]
     data_dir = DATA_ROOT.joinpath(*folder_genealogy)
     ds = HarpData.from_folder(data_dir, verbose=False)
-    assert len(ds) == 2
+    assert len(ds) == 1
     ds_name = "PZAD9.4d_S20211102_R165821_SpheresPermTube_harpmessage"
     d = ds[ds_name]
     assert d.full_name == folder_genealogy[-1] + "_" + ds_name
