74fb69013dacf2a690bb0eacc86f05fcd990a23c
Antonin Blot <antonin.blot@crick.ac.uk>
2024-05-01 13:48:11 +0100

    [v0.3.11]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flexiznam",
-    version="v0.3.10",
+    version="v0.3.11",
     url="https://github.com/znamlab/flexznam",
     license="MIT",
     author="Antonin Blot",
