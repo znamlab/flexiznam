b47984a641f1de8d8552765f5b2b0bda1f8fbada
Antonin Blot <antonin.blot@crick.ac.uk>
2024-12-13 17:05:05 +0000

    [dev] add pyproject.toml

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Don't forget to activate your virtual environment before running this script
-# install dependencies
-pip install -r requirements.txt
-# install flexiznam
-pip install -e .
-# configure
-flexiznam config
