4c73cb0421e34d1b4ab7f17d604768de066bb9d3
Antonin Blot <antonin.blot@crick.ac.uk>
2024-12-14 13:08:39 +0000

    [minor] spelling

--- tests/test_components/test_cli.py
+++ tests/test_components/test_cli.py
@@ -49,7 +49,7 @@ def test_add_password(tmp_path):
             "--app",
             "test_app",
             "--username",
-            "noone",
+            "no_one",
             "--password",
             "1234",
         ],
@@ -57,7 +57,7 @@ def test_add_password(tmp_path):
     assert result.exit_code == 0
     assert result.output.startswith("Password added in")
     p = config_tools.get_password(
-        username="noone", app="test_app", password_file=pwd_file
+        username="no_one", app="test_app", password_file=pwd_file
     )
     assert p == "1234"
 
